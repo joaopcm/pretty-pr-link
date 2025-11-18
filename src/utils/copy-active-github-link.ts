0e1129af9686267dd2c6029b73ea35c30a3c1867
João Melo <jopcmelo@gmail.com>
2025-11-18 20:01:36 -0300

    chore: lint

--- src/utils/copy-active-github-link.ts
+++ src/utils/copy-active-github-link.ts
@@ -2,33 +2,33 @@ import { BrowserExtension, Clipboard, showHUD } from "@raycast/api";
 import { showFailureToast } from "@raycast/utils";
 
 interface CopyActiveGithubLinkProps {
-	prefix: string | null;
+  prefix: string | null;
 }
 
 export async function copyActiveGithubLink({
-	prefix = null,
+  prefix = null,
 }: CopyActiveGithubLinkProps) {
-	const openTabs = await BrowserExtension.getTabs();
-	const activeTab = openTabs.find((tab) => tab.active);
+  const openTabs = await BrowserExtension.getTabs();
+  const activeTab = openTabs.find((tab) => tab.active);
 
-	if (!activeTab) {
-		await showFailureToast("No active tab found");
-		return;
-	}
+  if (!activeTab) {
+    await showFailureToast("No active tab found");
+    return;
+  }
 
-	if (!activeTab.url.includes("github.com")) {
-		await showFailureToast("The active tab is not a GitHub page");
-		return;
-	}
+  if (!activeTab.url.includes("github.com")) {
+    await showFailureToast("The active tab is not a GitHub page");
+    return;
+  }
 
-	const prTitle = await BrowserExtension.getContent({
-		tabId: activeTab.id,
-		format: "text",
-		cssSelector: ".js-issue-title.markdown-title",
-	});
+  const prTitle = await BrowserExtension.getContent({
+    tabId: activeTab.id,
+    format: "text",
+    cssSelector: ".js-issue-title.markdown-title",
+  });
 
-	const formattedLink = `${prefix ? `${prefix} ` : ""}[${prTitle}](${activeTab.url})`;
-	await Clipboard.copy(formattedLink);
+  const formattedLink = `${prefix ? `${prefix} ` : ""}[${prTitle}](${activeTab.url})`;
+  await Clipboard.copy(formattedLink);
 
-	await showHUD("Copied formatted link to clipboard 🥳");
+  await showHUD("Copied formatted link to clipboard 🥳");
 }
